c350c64762adf356234bf830cb578b5c6121d91a
Gian Ventura <106679444+piordaloud@users.noreply.github.com>
2023-09-18 10:40:36 -0300

    Add files via upload
    18/09

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-//Pom
-function tocaSomPom (IdElementoAudio) {
-   document.querySelector(IdElementoAudio).play();
+function tocaSom(idElementoAudio){
+   document.querySelector(idElementoAudio).play()
 }
 
-const listaDeTeclas = document.querySelectorAll ('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla")
 
-let contador = 0;
+let contador = 0
 
-//enquanto
-while (contador < listaDeTeclas.length) { 
-
-   const tecla = listaDeTeclas[contador];
+while (contador < listaDeTeclas.length){
+  
+   const tecla = listaDeTeclas[contador]
+  
    const instrumento = tecla.classList[1];
-   //template string
-   const idAudio = `#som_$[instrumento]`;
 
-   tecla.onclick = function () {
-      tocaSom(idAudio);
+   const idAudio = `#som_${instrumento}`
+
+   listaDeTeclas[contador].onclick = function () {
+       tocaSom(idAudio)
    }
 
    contador = contador + 1;
 
-  //console.log(contador);
-
+   console.log(contador);
 }
